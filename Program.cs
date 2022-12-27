3d5a7e04d42de1b13fd66ea54a463f02cdb0fce7
Sasha <a.magerova@list.ru>
2022-12-27 23:56:53 +0300

    1

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿/* Задача: Написать программу, которая из имеющегося массива строк формирует новый массив из строк, 
+длина которых меньше, либо равна 3 символам. */
+
+string[] array = new string [] { "Mama", "12", "Sister", "Bro", "52" };
+
+foreach (string i in array)
+{
+    Console.Write(i + " ");
+}
+
+
+
+
